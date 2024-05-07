8870b14731e06daf93371bf0b1925ccb1414f237
glubuchikd <glubuchikdenis@gmail.com>
2024-05-07 14:31:53 +0300

    new file "funcs" added, say_hello_by_name imported from funcs to main

---
+++ funcs.py
@@ -0,0 +1,2 @@
+def say_hello_by_name(name):
+    return f"Hello, {name}!"
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from funcs import say_hello_by_name
+
 print("Hello World")
 
 print("Hello World from ispo")
+
+print(say_hello_by_name("Denis"))
